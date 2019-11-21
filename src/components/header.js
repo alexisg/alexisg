822bad7815663b852d582b80326d478de7f9b5b3
Alexis Gallisa <alexis@alexisgallisa.com>
2019-11-20 21:46:02 -0800

    Move header component to use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
 import ProfileImage from "../images/pic216.jpg"
-import "./header.css"
+import AndreImage from "../images/icon-andre@2x.png"
+import styled from "styled-components"
+
+const Portrait = styled.div({
+  width: "9rem",
+  height: "9rem",
+  position: "relative" ,
+  margin: "0 auto",
+})
+
+const PortraitImage = styled.img({
+  width: "9rem",
+  height: "9rem",
+  borderRadius: "50%",
+  display: "block" 
+})
+
+const PortraitIcon = styled.div({
+  background: `url(${AndreImage}) top right no-repeat`,
+  backgroundSize: "24px auto",
+  position: "absolute",
+  bottom: "-8px",
+  right: "4px",
+  width: "24px",
+  height: "46px",
+  ":hover" : {
+    backgroundPosition: "0 -70px",
+  }
+})
 
 const Header = ({ siteTitle }) => (
-  <header className="header">
+  <header>
 
     <Link
       to="/"
     >
-      <div className="portrait transition">
-        <div className="portrait__img">
-          <img src={ProfileImage} alt="" />
-        </div>
-        <span className="portrait__icon icon icon--andre"></span>
-      </div>
-      <h1 className="header__logo align-center">
+      <Portrait>
+        <PortraitImage src={ProfileImage} alt="" />
+        <PortraitIcon />
+      </Portrait>
+      <h1>
         Alexis Gallis&aacute;
       </h1>
     </Link>
