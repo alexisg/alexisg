e7b7855c4804c0622a764bc258a4fe15a0c70125
Alexis Gallisa <alexis@alexisgallisa.com>
2020-08-11 09:42:52 -0700

    Re-implement sass compiling for css

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,12 @@
-// const pluginRss = require('@11ty/eleventy-plugin-rss')
-// const pluginNavigation = require('@11ty/eleventy-navigation')
-// const markdownIt = require('markdown-it')
-
-// const filters = require('./utils/filters.js')
-// const transforms = require('./utils/transforms.js')
-// const shortcodes = require('./utils/shortcodes.js')
-// const iconsprite = require('./utils/iconsprite.js')
-
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 
 
 module.exports = function (config) {
     // // Plugins
-    // config.addPlugin(pluginRss)
-    //config.addPlugin(pluginNavigation)
     config.addPlugin(inclusiveLangPlugin);
 
-    // // Filters
-    // Object.keys(filters).forEach((filterName) => {
-    //     config.addFilter(filterName, filters[filterName])
-    // })
-
-    // // Transforms
-    // Object.keys(transforms).forEach((transformName) => {
-    //     config.addTransform(transformName, transforms[transformName])
-    // })
-
-    // // Shortcodes
-    // Object.keys(shortcodes).forEach((shortcodeName) => {
-    //     config.addShortcode(shortcodeName, shortcodes[shortcodeName])
-    // })
-
-    // // Icon Sprite
-    // config.addNunjucksAsyncShortcode('iconsprite', iconsprite)
-
     // // Asset Watch Targets
-    // config.addWatchTarget('./src/static')
-
-    // // Markdown
-    // config.setLibrary(
-    //     'md',
-    //     markdownIt({
-    //         html: true,
-    //         breaks: true,
-    //         linkify: true,
-    //         typographer: true
-    //     })
-    // )
+    config.addWatchTarget('./src/sass')
 
     // Layouts
     config.addLayoutAlias('base', 'base.njk')
@@ -58,10 +18,6 @@ module.exports = function (config) {
     config.addPassthroughCopy('src/static/webfonts')
     config.addPassthroughCopy('src/static/css')
     config.addPassthroughCopy('src/static/js')
-
-
-    // Deep-Merge
-    // config.setDataDeepMerge(true)
 
     // Base Config
     return {
