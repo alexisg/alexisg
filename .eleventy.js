ee7f4970664aa7dd896c1efea0a70d61cb3312ca
Alexis Gallisá <alexis@alexisgallisa.com>
2025-04-08 11:04:44 -0400

    Upgrade to latest eleventy and sass (#24)
    * Update mixins to fix deprecated sass functions
    
    * Update dependencies to latest
    
    * Convert sass to scss
    
    * Update link to css
    
    * Update media queries

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,77 @@
-// const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language')
-const filters = require('./utils/filters.js')
-const postcss = require("postcss");
-const cssnano = require("cssnano");
-const eleventySass = require("eleventy-sass");
+const sass = require('sass');
+const path = require('path');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const filters = require('./utils/filters.js');
 
 module.exports = function (config) {
-    // // Plugins
-    // config.addPlugin(inclusiveLangPlugin);
-    config.addPlugin(eleventySass, {
-        postcss: postcss([cssnano])
-      });
+    // Handle Sass files
+    config.addTemplateFormats("sass");
+    config.addTemplateFormats("scss");
+    
+    config.addExtension("sass", {
+        outputFileExtension: "css",
+        compile: async function(inputContent, inputPath) {
+            if (path.basename(inputPath).startsWith("_")) {
+                return;
+            }
+            
+            let result = sass.compileString(inputContent, {
+                loadPaths: [
+                    path.dirname(inputPath),
+                    "node_modules"
+                ],
+                style: "compressed"
+            });
+            
+            // Process with PostCSS
+            const postCssResult = await postcss([cssnano]).process(result.css, { from: undefined });
+            
+            return async () => postCssResult.css;
+        }
+    });
+
+    config.addExtension("scss", {
+        outputFileExtension: "css",
+        compile: async function(inputContent, inputPath) {
+            if (path.basename(inputPath).startsWith("_")) {
+                return;
+            }
+            
+            let result = sass.compileString(inputContent, {
+                loadPaths: [
+                    path.dirname(inputPath),
+                    "node_modules"
+                ],
+                style: "compressed"
+            });
+            
+            // Process with PostCSS
+            const postCssResult = await postcss([cssnano]).process(result.css, { from: undefined });
+            
+            return async () => postCssResult.css;
+        }
+    });
 
     // Filters
     Object.keys(filters).forEach((filterName) => {
         config.addFilter(filterName, filters[filterName])
-    })
+    });
 
-    // // Asset Watch Targets
-    config.addWatchTarget('./src/sass')
+    // Asset Watch Targets
+    config.addWatchTarget('./src/sass');
 
     // Layouts
-    config.addLayoutAlias('base', 'base.njk')
-    config.addLayoutAlias('post', 'portfolio.njk')
+    config.addLayoutAlias('base', 'base.njk');
+    config.addLayoutAlias('post', 'portfolio.njk');
 
     // Pass-through files
-    config.addPassthroughCopy('src/static/images')
-    config.addPassthroughCopy('src/static/media')
-    config.addPassthroughCopy('src/static/webfonts')
-    config.addPassthroughCopy('src/static/css')
-    config.addPassthroughCopy('src/static/js')
-    config.addPassthroughCopy('src/static/api')
+    config.addPassthroughCopy('src/static/images');
+    config.addPassthroughCopy('src/static/media');
+    config.addPassthroughCopy('src/static/webfonts');
+    config.addPassthroughCopy('src/static/css');
+    config.addPassthroughCopy('src/static/js');
+    config.addPassthroughCopy('src/static/api');
 
     // Base Config
     return {
@@ -43,5 +85,5 @@ module.exports = function (config) {
         templateFormats: ['njk', 'md', '11ty.js'],
         htmlTemplateEngine: 'njk',
         markdownTemplateEngine: 'njk'
-    }
-}
+    };
+};
